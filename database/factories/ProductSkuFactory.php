70c05c7372fe17814421f8e35675cdad98ad1209
i65 <2461585309@qq.com>
2019-02-23 09:54:10 +0000

    用户界面 - 商品列表

--- database/factories/ProductSkuFactory.php
+++ database/factories/ProductSkuFactory.php
@@ -2,8 +2,11 @@
 
 use Faker\Generator as Faker;
 
-$factory->define(App\ProductSku::class, function (Faker $faker) {
+$factory->define(App\Models\ProductSku::class, function (Faker $faker) {
     return [
-        //
+        'title' => $faker->word,
+        'description' => $faker->sentence,
+        'price' => $faker->randomNumber(4),
+        'stock' => $faker->randomNumber(5),
     ];
 });
--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,9 @@
 |
 */
 
-Route::get('/', 'PagesController@root')->name('root');
+// Route::get('/', 'PagesController@root')->name('root');
+Route::redirect('/', '/products')->name('root');
+Route::get('products', 'ProductsController@index')->name('products.index');
 
 Auth::routes(['verify' => true]);
 
