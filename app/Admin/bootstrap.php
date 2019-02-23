de15afd05de239c11cc899e44dbfdc26df7c1c12
i65 <2461585309@qq.com>
2019-02-23 09:09:24 +0000

    管理后台 - 新增、编辑商品

--- app/Admin/bootstrap.php
+++ app/Admin/bootstrap.php
@@ -18,4 +18,8 @@
  *
  */
 
-Encore\Admin\Form::forget(['map', 'editor']);
+// Laravel-Admin 为了避免加载太多前端静态文件，
+// 默认禁用了 editor 这个表单组件，我们可以在 app/Admin/bootstrap.php 中把这个禁用解除：
+// Encore\Admin\Form::forget(['map', 'editor']);
+
+Encore\Admin\Form::forget(['map']);
