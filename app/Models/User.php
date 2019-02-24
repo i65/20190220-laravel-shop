3f2bbc93b2846d1f1db007e53d8d4dc051083010
i65 <2461585309@qq.com>
2019-02-24 03:29:22 +0000

    用户界面 - 商品收藏

--- app/Models/User.php
+++ app/Models/User.php
@@ -33,4 +33,12 @@ class User extends Authenticatable implements MustVerifyEmail
     {
         return $this->hasMany(UserAddress::class);
     }
+
+    // 与商品的关联
+    public function favoriteProducts()
+    {
+        return $this->belongsToMany(Product::class, 'user_favorite_products')
+                ->withTimestamps()
+                ->orderBy('user_favorite_products.created_at', 'desc');
+    }
 }
