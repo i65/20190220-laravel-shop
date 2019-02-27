ac831b2345c8b364921c37d17f17921c47c8c024
i65 <2461585309@qq.com>
2019-02-27 03:46:14 +0000

    引入支付库

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Monolog\Logger;
+use Yansongda\Pay\Pay;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,33 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        // 往服务容器中注入一个名为 alipay 的单例对象
+        $this->app->singleton('alipay', function () {
+            $config = config('app.alipay');
+            // 判断当前项目运行环境是否为线上环境
+            if (app()->environment() !== 'production') {
+                // 开发模式 
+                $config['mode'] = 'dev';
+                $config['log']['level'] = Logger::DEBUG;
+            } else {
+                $config['log']['level'] = Logger::WARNING;
+            }
+
+            // 调用 Yansongda\Pay 来创建一个支付宝支付对象
+            return Pay::alipay($config);
+        });
+
+        $this->app->singleton('wechat_pay', function () {
+            $config = config('app.wechat');
+            // 判断当前项目运行环境是否为线上环境
+            if (app()->environment() !== 'production') {                
+                $config['log']['level'] = Logger::DEBUG;
+            } else {
+                $config['log']['level'] = Logger::WARNING;
+            }
+
+            // 调用 Yansongda\Pay 来创建一个支付宝支付对象
+            return Pay::wechat($config);
+        });
     }
 }
