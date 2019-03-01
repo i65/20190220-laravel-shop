2e72e3ada73b18f6ccbfe779dfa8c11b47aa27b1
i65 <2461585309@qq.com>
2019-03-01 06:47:46 +0000

    管理后台 - 订单发货

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -18,6 +18,10 @@ Route::group([
     $router->get('products/{id}/edit', 'ProductsController@edit');
     $router->put('products/{id}', 'ProductsController@update');
 
+    // 订单列表路由
     $router->get('orders', 'OrdersController@index')->name('admin.orders.index');
+    // 订单详情路由
     $router->get('orders/{order}', 'OrdersController@show')->name('admin.orders.show');
+    // 发货路由
+    $router->post('orders/{order}/ship', 'OrdersController@ship')->name('admin.orders.ship');
 });
